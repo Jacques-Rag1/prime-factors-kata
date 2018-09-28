a81291a48e4a62f12361f746e22a70d7bb9387b5
Jacques <jak.rag@gmail.com>
2018-09-28 16:23:32 +0200

    create method primeFactors and test for 2

--- src/main/java/PrimeFactors.java
+++ src/main/java/PrimeFactors.java
@@ -1,2 +1,5 @@
-class PrimeFactors {
+public class PrimeFactors {
+    public static int[] primeFactors(int number) {
+        return new int[]{2};
+    }
 }
--- src/test/java/PrimeFactorsTest.java
+++ src/test/java/PrimeFactorsTest.java
@@ -1,5 +1,13 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
-class PrimeFactorsTest {
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PrimeFactorsTest {
+
+    @Test
+    public void primeFactors_should_return_list_with_2_for_2() {
+        int[] expected = {2};
+        assertThat(PrimeFactors.primeFactors(2)).isEqualTo(expected);
+    }
 
 }
